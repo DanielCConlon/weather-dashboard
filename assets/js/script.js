19ac87e74fb665f7c80b889a5f610eb1b2adb936
Daniel Conlon <danielcconlon@gmail.com>
2022-05-26 23:01:51 -0400

    added weather apikey

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 var currentDayWeather = document.querySelector("#current-date-weather");
 var weekForecast = document.querySelector("#week-farecast");
 
+var apiKey = "9e906a9a12e5dcce79a9f9095d702e4f"
+
 var getCityWeather = function() {
     // hard coded for cary nc longitude and latititude
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={35.7915}&lon={78.7811}&exclude={part}";
